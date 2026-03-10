253d60925551fe5c1e6bca9ddd36981b1eadbfdb
raf4570 <rafael.martins.hipolito@gmail.com>
2026-03-10 12:21:08 +0000

    fuck this comit

--- TicketMachine_VHDL/KeyDecode.vhd
+++ TicketMachine_VHDL/KeyDecode.vhd
@@ -5,12 +5,15 @@ entity KeyDecode is
 	port(
 		Clk	: in std_logic;
 		Reset	: in std_logic;
-		KbdLin: in std_logic_vector(4 downto 1);
+		KbdLin: in std_logic_vector(3 downto 0);
 		Tdelay: in std_logic_vector(1 downto 0);
 		Kack	: in std_logic;
-		KbdCol: out std_logic_vector(4 downto 1);
+		KbdCol: out std_logic_vector(3 downto 0);
 		Kval	: out std_logic;
-		Kcode	: out std_logic_vector(3 downto 0)
+		Kcode	: out std_logic_vector(3 downto 0);
+--		for test
+		Kpress: out std_logic;
+		Kscan: in std_logic_vector(1 downto 0)
 	);
 end KeyDecode;
 
@@ -18,19 +21,17 @@ architecture structural of KeyDecode is
 
 component KeyScan is
 	port(
-		Clk, rst, Kscan: in std_logic;
-		KbdLin: in std_logic_vector(4 downto 1);
-		KbdCol: out std_logic_vector(4 downto 1);
+		Clk: in std_logic;
+		rst: in std_logic;
+		Kscan: in std_logic_vector(1 downto 0);
+		KbdLin: in std_logic_vector(3 downto 0);
+		KbdCol: out std_logic_vector(3 downto 0);
 		K:	out std_logic_vector(3 downto 0);
 		Kpress: out std_logic
 	);
 end component KeyScan;
 
-signal Kscan:	std_logic;
-signal Kpress:	std_logic;
-
 begin
-	Kscan <= '0';
 	Scan	: KeyScan		port map(Clk => Clk, rst => Reset, Kscan => Kscan, KbdLin => KbdLin, KbdCol => KbdCol, K => Kcode, Kpress => Kpress);
 	--Ctrl	: KeyControl	port map();
 end structural;
